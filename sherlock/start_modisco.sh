7d28c434cf36ac7eafabbacefb1e90b7ed9afe0f
Anusri Pampari <anusri@stanford.edu>
2022-01-18 13:39:15 -0800

    modisco run start fixed

--- sherlock/start_modisco.sh
+++ sherlock/start_modisco.sh
@@ -12,21 +12,24 @@ oak_dir=$4
 
 if [[ -d $output_dir/$experiment ]] ; then
     if [[ -f $output_dir/$experiment/chrombpnet_model/interpret/$experiment.profile_scores.h5 ]] ; then
+        if [[ -f $oak_dir/$experiment/modisco_results_allChroms_profile.hdf5 ]] ; then
+            echo "modisco run exists"
+        else
+            mkdir $oak_dir/$experiment
+            scores_prefix=$output_dir/$experiment/chrombpnet_model/interpret/$experiment
+            modisco_dir=$oak_dir/$experiment
+            cell_type=$experiment  
 
-        mkdir $oak_dir/$experiment
-        scores_prefix=$output_dir/$experiment/chrombpnet_model/interpret/$experiment
-        modisco_dir=$oak_dir/$experiment
-        cell_type=$experiment  
-
-        cores=20
-        score_type=profile
-        sbatch --export=ALL --requeue \
-            -J $experiment.modisco \
-            -p owners,akundaje \
-            -t 720 -c $cores --mem=150G \
-            -o $modisco_dir/profile.file.log.o \
-            -e $modisco_dir/profile.file.log.e \
-             run_modisco.sh  $scores_prefix $modisco_dir $score_type $cell_type $output_dir/$experiment/
+            cores=20
+            score_type=profile
+            sbatch --export=ALL --requeue \
+                -J $experiment.modisco \
+                -p owners,akundaje \
+                -t 720 -c $cores --mem=150G \
+                -o $modisco_dir/profile.file.log.o \
+                -e $modisco_dir/profile.file.log.e \
+                run_modisco.sh  $scores_prefix $modisco_dir $score_type $cell_type $output_dir/$experiment/
+        fi
     else
         echo "do interpretation step first"
     fi
