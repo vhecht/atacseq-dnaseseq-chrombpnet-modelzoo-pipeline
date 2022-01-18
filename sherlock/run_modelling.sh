906df2c3019b2e5dfc2a3fedee5b56ddd6b4675b
Anusri Pampari <anusri@stanford.edu>
2022-01-18 14:12:27 -0800

    modelling to bias model as argumnet

--- sherlock/run_modelling.sh
+++ sherlock/run_modelling.sh
@@ -4,15 +4,18 @@
 experiment=$1
 dir=$2
 oak_dir=$3
+bias_model=$4
 
 cd chrombpnet
+
+cp -r $oak_dir/preprocessing $dir
+cp -r $oak_dir/negatives_data $dir
 
 reference_fasta=/scratch/groups/akundaje/anusri/chromatin_atlas/reference/hg38.genome.fa
 bigwig_path=$dir/preprocessing/bigWigs/$experiment.bigWig
 overlap_peak=$dir/preprocessing/downloads/peaks.bed.gz
 nonpeaks=$dir/negatives_data/negatives_with_summit.bed
 fold=/scratch/groups/akundaje/anusri/chromatin_atlas/splits/fold_0.json
-bias_model=/scratch/groups/akundaje/anusri/chromatin_atlas/reference/bias.h5
 output_dir=$dir/chrombpnet_model/
 pipeline_json=$dir/preprocessing/params_file.json
 assay_type=`jq .assay_type $pipeline_json | sed 's/"//g'`
