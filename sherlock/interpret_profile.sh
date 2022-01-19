7e9562c53c6c7d250bd67b729b2c6e93f18688b2
Anusri Pampari <anusri@stanford.edu>
2022-01-18 16:04:54 -0800

    profile interpret

---
+++ sherlock/interpret_profile.sh
@@ -0,0 +1,27 @@
+reference_fasta=$1
+regions=$2
+output_prefix=$3
+model_h5=$4
+logfile=$5
+
+function timestamp {
+    # Function to get the current time with the new line character
+    # removed
+
+    # current time
+    date +"%Y-%m-%d_%H-%M-%S" | tr -d '\n'
+}
+
+# create the log file
+if [ -z "$logfile" ]
+  then
+    echo "No logfile supplied - creating one"
+    logfile=$output_prefix".interpet.log"
+    touch $logfile
+fi
+
+
+
+
+echo $( timestamp ): "python $PWD/src/evaluation/interpret/interpret.py --genome=$reference_fasta --regions=$regions --output_prefix=$output_prefix --model_h5=$model_h5 --profile_or_counts profile" | tee -a $logfile
+python $PWD/src/evaluation/interpret/interpret.py --genome=$reference_fasta --regions=$regions --output_prefix=$output_prefix --model_h5=$model_h5 --profile_or_counts profile
